3868d11caaa98da27e3576d1a093249d305ad5e2
Emilio Bellini <emilio.bellini@gmail.com>
2016-12-12 11:16:21 +0000

    Added assumption to neglect solutions with primescalar[]==0

--- SVT.m
+++ SVT.m
@@ -46,7 +46,7 @@ Import[$CodeDirectory<>"AutomaticRules.m"]
 (****   Assumptions   ****)
 
 
-$Assumptions = {scale[]>0, hubbleC[]>0, hubbleP[]>0};
+$Assumptions = {scale[]>0, hubbleC[]>0, hubbleP[]>0, primescalar[]!=0};
 
 
 (****   SVT Expand Rules   ****)
