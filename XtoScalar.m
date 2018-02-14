a6a0dc4d6dfa74062ff0449c92b360217b8a9a81
Emilio Bellini <emilio.bellini@gmail.com>
2018-02-14 12:24:41 +0000

    Corrected bug in X

--- XtoScalar.m
+++ XtoScalar.m
@@ -8,8 +8,13 @@ X1=Xcov[] //.XtoScalarrules // Decomposition[1];
 X2=Xcov[] //.XtoScalarrules // Decomposition[2];
 
 
+X1cov = Perturbation[Xcov[] //.XtoScalarrules,1] // ExpandPerturbation
+X2cov = Perturbation[Xcov[] //.XtoScalarrules,2] // ExpandPerturbation
+
+
 XtoScalarrules = Flatten[{MakeRule[{X[],Evaluate[X0]}],
-	MakeRule[{pertX[LI[1]],Evaluate[X1]}],MakeRule[{pertX[LI[2]],Evaluate[X2]}]}];
+	MakeRule[{pertX[LI[1]],Evaluate[X1]}],MakeRule[{pertX[LI[2]],Evaluate[X2]}],
+	MakeRule[{pertXcov[LI[1]],Evaluate[X1cov]}],MakeRule[{pertXcov[LI[2]],Evaluate[X2cov]}]}];
 
 
 XtoScalar[expr_] := expr //.XtoScalarrules //.fun_[scalar[],X0]:>fun[scalar[],X[]] // ToCanonical // ReplaceDummies
