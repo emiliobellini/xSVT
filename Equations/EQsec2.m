9f31619d466c8b4be311acc173fb32e3bc1dfa65
Emilio Bellini <emilio.bellini@gmail.com>
2019-09-06 17:03:28 +0100

    Calculated second order Einstein 0i

---
+++ Equations/EQsec2.m
@@ -0,0 +1,22 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+-source2[LI[2]] - (2*hubbleC[]*metric\[Delta][p$24184329, p$24184330]*
+   PD[-p$24184329][PD[-p$24184330][pertpsi[LI[2]]]])/scale[] + 
+ (alphaB[]*hubbleC[]*metric\[Delta][p$24184329, p$24184330]*
+   PD[-p$24184329][PD[-p$24184330][pertpsi[LI[2]]]])/scale[] + 
+ (density[]*metric\[Delta][p$24184329, p$24184330]*
+   PD[-p$24184329][PD[-p$24184330][pertV[LI[2]]]])/mass2[] + 
+ (metric\[Delta][p$24184329, p$24184330]*pressure[]*
+   PD[-p$24184329][PD[-p$24184330][pertV[LI[2]]]])/mass2[] - 
+ (2*hubbleC[]^2*metric\[Delta][p$24184329, p$24184330]*
+   PD[-p$24184329][PD[-p$24184330][pertV[LI[2]]]])/scale[]^2 + 
+ (2*metric\[Delta][p$24184329, p$24184330]*primehubbleC[]*
+   PD[-p$24184329][PD[-p$24184330][pertV[LI[2]]]])/scale[]^2 - 
+ (density[]*metric\[Delta][p$24184329, p$24184330]*
+   PD[-p$24184329][PD[-p$24184330][pertvelocity[LI[2]]]])/mass2[] - 
+ (metric\[Delta][p$24184329, p$24184330]*pressure[]*
+   PD[-p$24184329][PD[-p$24184330][pertvelocity[LI[2]]]])/mass2[] - 
+ (2*metric\[Delta][p$24184329, p$24184330]*timevec[h$24184349]*
+   PD[-p$24184329][PD[-p$24184330][PD[-h$24184349][pertphi[LI[2]]]]])/
+  scale[] + (alphaB[]*hubbleC[]*metric\[Delta][p$24184329, p$24184330]*
+   timevec[h$24184349]*PD[-p$24184329][PD[-p$24184330][
+     PD[-h$24184349][pertV[LI[2]]]]])/scale[]^2
