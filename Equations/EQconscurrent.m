f2633a099605788bc57981444f06d7c9c7d15331
Emilio Bellini <emilio.bellini@protonmail.com>
2020-12-03 10:15:26 +0100

    Added shift/current and sound speed equations

---
+++ Equations/EQconscurrent.m
@@ -0,0 +1,4 @@
+(* ::Package:: *)
+
+(* Created with the Wolfram Language : www.wolfram.com *)
+(3*current[]*hubbleC[])/scale[] + primecurrent[]/scale[] - shift[]
---
+++ Equations/EQcs2.m
@@ -0,0 +1,9 @@
+(* ::Package:: *)
+
+(-2*(-2 + alphaB[])*alphaM[])/(3*alphaB[]^2 + 2*alphaK[]) - 
+ ((-2 + alphaB[])^2*alphaT[])/(3*alphaB[]^2 + 2*alphaK[]) - cs2[] + 
+ (2*primealphaB[])/((3*alphaB[]^2 + 2*alphaK[])*hubbleC[]) - 
+ ((-2 + alphaB[])*(2*hubbleC[]^2 + alphaB[]*hubbleC[]^2 - 2*primehubbleC[]))/
+  ((3*alphaB[]^2 + 2*alphaK[])*hubbleC[]^2) - 
+ (2*density[]*scale[]^2)/((3*alphaB[]^2 + 2*alphaK[])*hubbleC[]^2*mass2[]) - 
+ (2*pressure[]*scale[]^2)/((3*alphaB[]^2 + 2*alphaK[])*hubbleC[]^2*mass2[])
